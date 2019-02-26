b50e5d1fc2de592e582ad7b8350938cbdc7fbb70
Michal Mietlinski <mietlinski.michal.priv@gmail.com>
2019-02-26 17:55:34 +0100

    FAQFIX

--- fast-read/src/components/Faq.vue
+++ fast-read/src/components/Faq.vue
@@ -5,7 +5,7 @@
      <li v-for="(item, index) in copy" v-bind:key="index">
       <span v-on:click="open(index)">{{item.question}}</span><br/>
       <hr>
-      <span v-show="showarr[index]">{{item.answear}}</span>
+      <span v-show="item.opened">{{item.answear}}</span>
      </li>
    </ul>
  </div>
@@ -16,27 +16,21 @@ import Copy from '@/assets/copy';
 
 export default {
   name: 'Main',
-  props: {
-    msg: String,
-  },
   // computed: () => {
   //   result: this.showarr
   // },
   data: () => ({
     abc: 'an',
-    copy: Copy.faq,
-    showarr:[false],
+    copy: Copy.faq.map((el)=>{
+      return Object.assign(el, {opened:false})
+    }),
   }),
   methods: {
-    open: function(index) {
-      
-        this.$nextTick(()=>{this.showarr[0] = true});
-
-
-        if(this.showarr[index] !== true){
-          this.showarr[index]=true;
+    open: function(index)   {
+        if(this.copy[index].opened !== true){
+          this.copy[index].opened=true;
         }else{
-          this.showarr[index]=false;
+          this.copy[index].opened=false;
         }
     }
   }
