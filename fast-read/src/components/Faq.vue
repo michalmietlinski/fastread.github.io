d3114b2e5e3430676152510dfc79d1552c79b7ed
Aleksander Jakubowski <ol3kjak@gmail.com>
2019-02-26 16:18:12 +0100

    Copy.faq

--- fast-read/src/components/Faq.vue
+++ fast-read/src/components/Faq.vue
@@ -1,18 +1,25 @@
 <template>
  <div>
-   {{abc}}
+   <ul class="faq-css">
+     <li v-for="(item, index) in copy" v-bind:key="index">
+      {{item.question}} <br/> 
+      {{item.answear}} <hr>
+     </li>
+   </ul>
  </div>
 </template>
 
 <script>
 import Copy from '@/assets/copy';
+
 export default {
   name: 'Main',
   props: {
     msg: String,
   },
   data: () => ({
-    abc: 'nnaaan'
+    abc: 'an',
+    copy: Copy.faq
   }),
 
 };
