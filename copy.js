f75e248be070cedce16f6cd1f8e4ac2b8be8c257
Aleksander Jakubowski <ol3kjak@gmail.com>
2019-03-05 14:10:06 +0100

    aa

---
+++ copy.js
@@ -0,0 +1,100 @@
+const copy = {
+  English: {
+    menu: {
+      faq: 'FAQ',
+      contact: 'Contact',
+      jumper: 'Jumper',
+      timer: 'Timer',
+      home: 'Home',
+    },
+    section: {
+      counter: {
+        header: 'Check your reading speed',
+        describtion: 'Just input the text, click on start, and as soon as you are done with reading, click it again. You will see your results and progress from previous tests.',
+        inputtextdesc: 'Paste your chosen text here:',
+        timeOfReading: 'Time reading',
+        wordsPerMinute: 'Words per minute',
+        numberOfWords: 'Words in text',
+        latest: 'Latest',
+      },
+      jumper: {
+        header: 'Train your eyes',
+        describtion: 'This tool alows you to move your eyes a bit! Just input text, and follow displayed words with your eyes, without moving head. This way you\' force your muscles to work',
+      },
+      main: {
+        header: 'Read faster by making the screen do the work!',
+        describtion: 'This tool shows you text displaying it in one place, so you can eliminate jumping your eyes and loosing track of what you read, instead - focus on understanding!',
+      },
+    },
+    controls: {
+      speed: 'Speed',
+      words: 'Words',
+      rows: 'Rows',
+      stop: 'Stop',
+      start: 'Start',
+      restart: 'Restart',
+      rewind: 'Rewind',
+      reset: 'Reset',
+    },
+    faq: [
+      {
+        question: 'Question',
+        answear: 'Answear',
+      },
+      {
+        question: 'Question2',
+        answear: 'Answear2',
+      },
+    ],
+  },
+  Polski: {
+    menu: {
+      faq: 'FAQ',
+      contact: 'Kontakt',
+      jumper: 'Skoczek !',
+      timer: 'Czas',
+      home: 'Home',
+    },
+    section: {
+      counter: {
+        header: 'Sprawdź swoje obecną predkość / tepo czytania', // DECIDE 
+        describtion: 'Just input the text, click on start, and as soon as you are done with reading, click it again. You will see your results and progress from previous tests.',
+        inputtextdesc: 'Wklej swój teks tutaj:',
+        timeOfReading: 'Czas czytania',
+        wordsPerMinute: 'Slów na minute',
+        numberOfWords: 'Ilość slów w tekście',
+        latest: 'Latest',
+      },
+      jumper: {
+        header: 'Tranuj swój wzrok',
+        describtion: 'zabawka',
+      },
+      main: {
+        header: 'Czytaj szybciej dzięki pomocy aplikacji',
+        describtion: 'zabawka',
+      },
+    },
+    controls: {
+      speed: 'Prędkość',
+      words: 'Słowa',
+      rows: 'Rzędy',
+      stop: 'Stop',
+      start: 'Start',
+      restart: 'Restart',
+      rewind: 'Przewijanie',
+      reset: 'Reset',
+    },
+    faq: [
+      {
+        question: 'Pytsnie 1',
+        answear: 'Odpowiedź 1',
+      },
+      {
+        question: 'Pytsnie 2',
+        answear: 'Odpowiedź 2',
+      },
+    ],
+  },
+};
+
+export default copy;
